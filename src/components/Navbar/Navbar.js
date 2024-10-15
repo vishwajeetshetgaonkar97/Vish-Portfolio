99eda4a205b2bab2779a5d82e5eb118219628540
vishwajeetshetgaonkar97 <vishwajeet@sportvot.com>
2024-10-15 13:28:37 -0400

    Wip

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import React from "react";
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import closeIcon from '../../../public/close.png';
+import menuIcon from '../../../public/menu.png'
 
 const Navbar = ({ isProject = false }) => {
   const router = useRouter();
@@ -12,11 +15,12 @@ const Navbar = ({ isProject = false }) => {
       router.back(); // Goes back if it's not a project
     }
   };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <nav
-        className={`${styles.navbar} flex items-center content-center  w-full px-6 py-4 `}
+        className={`${styles.navbar} flex items-center content-center  w-full md:px-6 px-2 py-4 `}
       >
         <Image
          src={isProject?"/back.png":"/logo.png"}
@@ -24,9 +28,24 @@ const Navbar = ({ isProject = false }) => {
           width={isProject?25:50}
           alt="vishwajeet shetgaonkar logo"
           onClick={handleIconClick}
-          className="cursor-pointer"
+          className={`cursor-pointer ${styles.logo}`}
         />
-        <div className="flex items-end content-end w-full justify-end space-x-4">
+        <div
+        className={styles.linksBurgerContainer}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Image
+          className={`${isOpen ? styles.closeIcon : styles.burgerIcon}`}
+          src={isOpen ? closeIcon : menuIcon}
+          width={100}
+          height={100}
+          alt="logo image"
+        />
+      </div>
+
+        <div className={`${styles.navList} ${
+            isOpen ? styles.linksContainerActive : ""
+          }`}>
           <a className={styles.topNavButton}>Work</a>
           <a className={styles.topNavButton}>About</a>
           <a className={styles.topNavButton}>Contact</a>
